d230b3b153bb65627a027ded992b4cba04d2fd49
Areeba Shaikh <59220944+AreebaAkbar@users.noreply.github.com>
2021-04-06 10:34:45 -0700

    Add files via upload

---
+++ Lab6_HA.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab6_h.a;
+
+
+public class Lab6_HA {
+
+   
+    public static void main(String[] args) {
+        
+       Calculator.Multiply(1, 2);
+       Calculator.Modulus(6, 3);
+    }
+    
+}
